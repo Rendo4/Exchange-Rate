30a201debbe2189b8a8d3ea6225cda63c954bb96
Ryan Rendon <Rendon.S.Ryan@gmail.com>
2020-07-27 21:41:36 -0700

    updated ui with a button and began trying to make an api call

--- src/main.js
+++ src/main.js
@@ -5,7 +5,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-  $('#').submit(function(event) {
+  $('#convert').submit(function(event) {
     event.preventDefault();
+    const amount = $('#dollars').val();
+    const currency = $("#money").val();
+    $("#dollas").val("");
+    $("#money").val("");
+
+    let request = new XMLHttpRequest();
+    const url = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`;
+
+    request.onreadystatechange = function() {
+      if (this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+
+      request.open("GET", url, true);
+      request.send();
+
+      const getElements = function(response) {
+        $('.showResults').text('You would have this amount in this currency');
+      };
+    };
   });
 });
