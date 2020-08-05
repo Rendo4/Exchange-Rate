e79841f469c08616dc3f26f30c27fa6b05ae4fc8
Ryan Rendon <Rendon.S.Ryan@gmail.com>
2020-08-04 20:49:38 -0700

    changed code to have async and iffy functionality

--- src/main.js
+++ src/main.js
@@ -3,40 +3,72 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { money } from './exchange.js';
+// import { money } from './exchange.js';
+
+// $(document).ready(function() {
+//   $('#convert').click(function(event) {
+//     event.preventDefault();
+//     const amount = $('#number').val();
+//     const currency = $("#money").val();
+//     $("#numbers").val("");
+//     $("#money").val("");
+
+//     let promise = new Promise(function(resolve, reject) {
+//       let request = new XMLHttpRequest();
+//       const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+//       request.onload = function() {
+//         if (this.status === 200) {
+//           resolve(request.response);
+//         } else {
+//           reject(request.response);
+//         }
+//       };
+//       request.open("GET", url, true);
+//       request.send();
+//     });
+
+//     promise.then(function(response) {
+//       const body = JSON.parse(response);
+//       let country = 0;
+//       let unit = 0;
+//       $('.showResults').text(`You would have ${body.conversion_rates[currency] * amount} ${unit} in ${country}`);
+//       $('.showErrors').text("");
+//     }, function(error) {
+//       $('.showErrors').text(`There was an error processing your request: ${error}`);
+//       $('.showResults').text("");
+//     });
+//   });
+// });
 
 $(document).ready(function() {
-  $('#convert').click(function(event) {
+  $('#convert').click(function() {
     event.preventDefault();
     const amount = $('#number').val();
     const currency = $("#money").val();
     $("#numbers").val("");
     $("#money").val("");
 
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
+   (async () => {
+      try {
+        const response = await fetch( `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+        if (!response.ok) {
+          ShowError(response.statusText);
         } else {
-          reject(request.response);
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
-
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-      let country = 0;
-      let unit = 0;
-      money(amount, country, unit);
-      $('.showResults').text(`You would have ${body.conversion_rates[currency] * amount} ${unit} in ${country}`);
+          const jsonifiedResponse = await response.json();
+          showElements(jsonifiedResponse);
+       }
+      } catch(error) {
+        console.log(error);
+      }
+    }) ();
+    function showElements(response) {
+      $('.showResults').text(`You would have ${response.conversion_rates[currency] * amount} unit in country`);
       $('.showErrors').text("");
-    }, function(error) {
+    }
+    
+    function ShowError(error) {
       $('.showErrors').text(`There was an error processing your request: ${error}`);
       $('.showResults').text("");
-    });
+    }
   });
 });
